a016a7dfcbf67c8227f40bb702f89e22637776c5
Juan Felipe <juanfelipeariastabares007@gmail.com>
2020-10-31 12:11:36 -0500

    file sh this file save the commands line

---
+++ migration.sh
@@ -0,0 +1,5 @@
+npx mikro-orm migration:create   # Create new migration with current schema diff
+npx mikro-orm migration:up       # Migrate up to the latest version
+npx mikro-orm migration:down     # Migrate one step down
+npx mikro-orm migration:list     # List all executed migrations
+npx mikro-orm migration:pending  # List all pending migrations
